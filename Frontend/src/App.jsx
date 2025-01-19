2bfc7398dab0c88ac5fe13cdd816df165c67b755
minkhantkyaw21 <minkhantkyaw2732@gmail.com>
2025-01-19 19:42:47 +0630

    Backend Done!

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,16 +2,22 @@
 import Signup from "./components/Signup";
 import Courses from "./courses/Courses";
 import Home from "./home/Home";
-import { Routes,Route } from 'react-router-dom';
+import { useState } from "react";
+import { Routes,Route, Navigate } from 'react-router-dom';
+import {Toaster} from 'react-hot-toast'
 const App = () => {
+   const [authUser,setAuthUser] = useState()
+    console.log(authUser)
+  
   return (
     <>
     <div className="dark:bg-slate-900 dark:text-white">
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/course" element={<Courses />} />
+      <Route path="/course" element={authUser ? <Courses /> : <Navigate to='/signup' />} />
       <Route path="/signup" element={<Signup />} />
    </Routes>
+   <Toaster />
     </div>
    
     </>
--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -1,32 +1,52 @@
-import list from '../../public/list.json';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Cards from './Cards';
 import {Link} from 'react-router-dom';
 const Course = () => {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 ">
-        <div className="mt-28  justify-center text-center items-center"> 
-          <h1 className="text-2xl font-semibold md:text-4xl "> We&apos;r delighted to have you  
-            <span className="text-blue-500"> Here!:)</span></h1>
-            <p className="mt-12">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-              Hic ab pariatur sequi illo deserunt ipsam perspiciatis quisquam ex magni suscipit
-               alias et eveniet quidem, minima assumenda totam sint explicabo in.
-            </p>
-           <Link to='/'>
-           <button className="mt-6 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 duration-300">
-             Back</button>
-           </Link>
-            
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className="text-2xl  md:text-4xl">
+            We're delighted to have you{" "}
+            <span className="text-blue-500"> Here! :)</span>
+          </h1>
+          <p className="mt-12">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
+            assumenda? Repellendus, iste corrupti? Tempore laudantium
+            repellendus accusamus accusantium sed architecto odio, nisi expedita
+            quas quidem nesciunt debitis dolore non aspernatur praesentium
+            assumenda sint quibusdam, perspiciatis, explicabo sequi fugiat amet
+            animi eos aut. Nobis quisquam reiciendis sunt quis sed magnam
+            consequatur!
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 duration-300">
+              Back
+            </button>
+          </Link>
         </div>
-        <div className='mt-12 grid grid-cols-1 md:grid-cols-4'>
-          {list.map((item)=>(
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {book.map((item) => (
             <Cards key={item.id} item={item} />
           ))}
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Course
